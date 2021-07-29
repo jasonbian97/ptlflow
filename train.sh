8f855dd2c17680173ec88a5a92e94537e3af7891
ZhangxingBian <jasonbian.zx@gmail.com>
2021-07-28 22:42:12 -0400

    a

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python train.py raft umserver --train_dataset chairs-train --val_dataset chairs-val --lr 0.0001 --train_batch_size 4 --max_epochs 10 --gpus 4
+python train.py raft umserver --train_dataset chairs-train --val_dataset chairs-val --lr 0.0001 --train_batch_size 4 --max_epochs 10 --gpus "4"
